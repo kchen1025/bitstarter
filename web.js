cb2b35c58d7d08636a30893465de75f8e68b50fd
kchen1025 <kchen1025@gmail.com>
2014-03-25 13:26:26 -0700

    changes to web.js

--- web.js
+++ web.js
@@ -2,8 +2,16 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+
+var dummy = fs.readFileSync(index.html,String);
+
+var buffer = new Buffer(80);
+buffer.write(dummy,"utf-8");
+var output = buffer.toString("utf-8",0,20);
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World! 2');
+  response.send(output);
 });
 
 var port = process.env.PORT || 5000;
