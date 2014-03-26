c9d76b40d57edee81eb9927ba95a85d7b437cae2
kchen1025 <kchen1025@gmail.com>
2014-03-25 19:26:24 -0700

    web.js

--- web.js
+++ web.js
@@ -3,16 +3,17 @@ var app = express();
 app.use(express.logger());
 
 
-/*var fs = require('fs');
-var dummy = fs.readFileSync(index.html,String);
-
+var fs = require('fs');
 var buffer = new Buffer(80);
-buffer.write(dummy,"utf-8");
-var output = buffer.toString("utf-8",0,80);*/
+
+buffer = fs.readFileSync(index.html,null);
+
+//buffer.write(dummy,"utf-8");
+var output = buffer.toString("utf-8",0,80);
 
 
 app.get('/', function(request, response) {
-  response.send("hi there");
+  response.send(output);
 });
 
 var port = process.env.PORT || 5000;
